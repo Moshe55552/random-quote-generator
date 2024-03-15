32de41b8a84ca7e8e4683ef46371c208a9735cfa
Moshe55552 <80269537+Moshe55552@users.noreply.github.com>
2024-03-15 06:07:40 +0200

    src

---
+++ App.js
@@ -0,0 +1,13 @@
+import RandomQuote from './Component/RandomQuete/RandomQuote';
+
+function App() {
+  return (
+    <div className="App">
+     <RandomQuote>
+      
+     </RandomQuote>
+    </div>
+  );
+}
+
+export default App;
